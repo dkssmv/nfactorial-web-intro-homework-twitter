9ad866a5327d3e8f6a40b863ccae7600b016a999
Darina Kassymova <darinakassymova@MacBook-Air-Darina.local>
2023-05-23 01:43:17 +0600

    first

--- src/components/HomeComponent/NewTweet/index.js
+++ src/components/HomeComponent/NewTweet/index.js
@@ -2,7 +2,7 @@ import { PROFILE_IMG_PATH } from "../images";
 
 export default function NewTweet(){
     return (
-        <div style={{borderBottom: '2px solid whitesmoke'}}>
+        <div className='p-3' style={{borderBottom: '2px solid whitesmoke'}}>
             <img src={PROFILE_IMG_PATH} style={{width: 50, height: 50, borderRadius: 50}}/>
             <input placeholder='Whats happening?' style={{border:'none', fontSize:18, outline:'none'}} className='mx-3' />
 
